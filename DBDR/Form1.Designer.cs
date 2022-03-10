3a9f387089e449919c28a6538d3c092893e93258
PK1 <npl1u1pc@mail.ru>
2022-03-10 14:24:51 +0700

    Добавьте файлы проекта.

---
+++ DBDR/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿
+namespace DBDR
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbName = new System.Windows.Forms.TextBox();
+            this.tbPassw = new System.Windows.Forms.TextBox();
+            this.lbName = new System.Windows.Forms.Label();
+            this.lbPassw = new System.Windows.Forms.Label();
+            this.btnLogIn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // tbName
+            // 
+            this.tbName.Location = new System.Drawing.Point(72, 13);
+            this.tbName.Name = "tbName";
+            this.tbName.Size = new System.Drawing.Size(198, 20);
+            this.tbName.TabIndex = 0;
+            // 
+            // tbPassw
+            // 
+            this.tbPassw.Location = new System.Drawing.Point(72, 39);
+            this.tbPassw.Name = "tbPassw";
+            this.tbPassw.PasswordChar = '*';
+            this.tbPassw.Size = new System.Drawing.Size(198, 20);
+            this.tbPassw.TabIndex = 1;
+            // 
+            // lbName
+            // 
+            this.lbName.AutoSize = true;
+            this.lbName.Location = new System.Drawing.Point(12, 16);
+            this.lbName.Name = "lbName";
+            this.lbName.Size = new System.Drawing.Size(35, 13);
+            this.lbName.TabIndex = 2;
+            this.lbName.Text = "Name";
+            // 
+            // lbPassw
+            // 
+            this.lbPassw.AutoSize = true;
+            this.lbPassw.Location = new System.Drawing.Point(12, 42);
+            this.lbPassw.Name = "lbPassw";
+            this.lbPassw.Size = new System.Drawing.Size(53, 13);
+            this.lbPassw.TabIndex = 3;
+            this.lbPassw.Text = "Password";
+            // 
+            // btnLogIn
+            // 
+            this.btnLogIn.Location = new System.Drawing.Point(102, 65);
+            this.btnLogIn.Name = "btnLogIn";
+            this.btnLogIn.Size = new System.Drawing.Size(75, 23);
+            this.btnLogIn.TabIndex = 4;
+            this.btnLogIn.Text = "Log In";
+            this.btnLogIn.UseVisualStyleBackColor = true;
+            this.btnLogIn.Click += new System.EventHandler(this.btnLogIn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(288, 120);
+            this.Controls.Add(this.btnLogIn);
+            this.Controls.Add(this.lbPassw);
+            this.Controls.Add(this.lbName);
+            this.Controls.Add(this.tbPassw);
+            this.Controls.Add(this.tbName);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbName;
+        private System.Windows.Forms.TextBox tbPassw;
+        private System.Windows.Forms.Label lbName;
+        private System.Windows.Forms.Label lbPassw;
+        private System.Windows.Forms.Button btnLogIn;
+    }
+}
+
---
+++ DBDR/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace DBDR
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection sqlconn;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLogIn_Click(object sender, EventArgs e)
+        {
+            string myselectquery = "SELECT * FROM [dbo].[DRTable] WHERE [Name]= '" + tbName.Text + "' and [Password]='" + tbPassw.Text + "'";
+
+
+            using (SqlDataAdapter dataAdapter = new SqlDataAdapter(myselectquery, sqlconn))
+            {
+                DataTable table = new DataTable();
+
+                dataAdapter.Fill(table);//Вылетает ошибка
+
+                if (table.Rows.Count == 0)
+                {
+                    MessageBox.Show("Ошибка, вы ввели неправильное имя, либо фамиилию, либо отчество");
+                }
+                else
+                {
+                    MessageBox.Show("Добро пожаловать " + table.Rows[0][1].ToString(), "Сообщение", MessageBoxButtons.YesNo, MessageBoxIcon.Information, MessageBoxDefaultButton.Button1,
+        MessageBoxOptions.DefaultDesktopOnly);
+
+
+
+                }
+            }
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string connect = @"Data Source = NPK107K1\SQLEXPRESS; Initial Catalog = DelsinRowe26; Integrated Security = True";
+            sqlconn = new SqlConnection(connect);
+        }
+    }
+}
